0d78f69ce9ab157dbec6668916d3aa996ab8d01e
Sarang Parikh <sarangparikh22@gmail.com>
2021-12-11 00:03:39 +0530

    updated interfaces to fixed solidity version

--- contracts/interfaces/IBentoBoxMinimal.sol
+++ contracts/interfaces/IBentoBoxMinimal.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity >=0.8.0;
+pragma solidity 0.8.10;
 
 /// @notice Minimal BentoBox vault interface.
 /// @dev `token` is aliased as `address` from `IERC20` for simplicity.
--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity 0.8.10;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
