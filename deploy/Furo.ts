b6ee39df80ad18e403c0d83ebe554d7b15f6bff8
Sarang Parikh <sarangparikh22@gmail.com>
2022-07-12 17:03:59 +0400

    added test deploy script

--- deploy/Furo.ts
+++ deploy/Furo.ts
@@ -1,18 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-const BENTO_ADDRESS = new Map();
-const SUSHI_FACTORY = new Map();
-const PAIR_CODE_HASH = new Map();
-const WNATIVE = new Map();
-
-BENTO_ADDRESS.set(137, "0x0319000133d3AdA02600f0875d2cf03D442C3367");
-SUSHI_FACTORY.set(137, "0xc35DADB65012eC5796536bD9864eD8773aBc74C4");
-WNATIVE.set(137, "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270");
-PAIR_CODE_HASH.set(
-  137,
-  "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303"
-);
-
 export default async (hre: HardhatRuntimeEnvironment) => {
   const accounts = await hre.ethers.getSigners();
   const { deployments } = hre;
@@ -22,41 +9,48 @@ export default async (hre: HardhatRuntimeEnvironment) => {
 
   const deployer = accounts[0].address;
 
-  const furo = await deploy("Furo", {
+  const WETH = await deploy("ERC20Mock", {
     from: deployer,
-    args: [BENTO_ADDRESS.get(chainId), WNATIVE.get(chainId)],
-  });
+    args: ["Wrapped ETH", "WETH", hre.ethers.utils.parseEther("1000000")]
+  })
 
-  console.log(`Furo deployed to ${furo.address}`);
+  console.log(`WETH deployed to ${WETH.address}`);
 
-  const swapReceiver = await deploy("SwapReceiver", {
+  const bentoBox = await deploy("BentoBoxV1", {
     from: deployer,
-    args: [
-      SUSHI_FACTORY.get(chainId),
-      BENTO_ADDRESS.get(chainId),
-      PAIR_CODE_HASH.get(chainId),
-    ],
+    args: [WETH.address]
+  })
+
+  console.log(`BentoBoxV1 deployed to ${bentoBox.address}`);
+
+  const furoStream = await deploy("FuroStream", {
+    from: deployer,
+    args: [bentoBox.address, WETH.address],
   });
 
-  console.log(`Swap Receiver to ${swapReceiver.address}`);
+  console.log(`FuroStream deployed to ${furoStream.address}`);
 
-  console.log(`Whitelisting Swap Receiver...`);
-  const furoContract = await hre.ethers.getContract("Furo");
-  await furoContract.whitelistReceiver(swapReceiver.address, true);
 
-  console.log(`Verifying Contracts...`);
-
-  await hre.run("verify:verify", {
-    address: furo.address,
-    constructorArguments: [BENTO_ADDRESS.get(chainId), WNATIVE.get(chainId)],
+  const furoVesting = await deploy("FuroVesting", {
+    from: deployer,
+    args: [bentoBox.address, WETH.address],
   });
 
-  await hre.run("verify:verify", {
-    address: swapReceiver.address,
-    constructorArguments: [
-      SUSHI_FACTORY.get(chainId),
-      BENTO_ADDRESS.get(chainId),
-      PAIR_CODE_HASH.get(chainId),
-    ],
-  });
+  console.log(`FuroVesting deployed to ${furoVesting.address}`);
+
+  // console.log(`Verifying Contracts...`);
+
+  // await hre.run("verify:verify", {
+  //   address: furo.address,
+  //   constructorArguments: [BENTO_ADDRESS.get(chainId), WNATIVE.get(chainId)],
+  // });
+
+  // await hre.run("verify:verify", {
+  //   address: swapReceiver.address,
+  //   constructorArguments: [
+  //     SUSHI_FACTORY.get(chainId),
+  //     BENTO_ADDRESS.get(chainId),
+  //     PAIR_CODE_HASH.get(chainId),
+  //   ],
+  // });
 };
