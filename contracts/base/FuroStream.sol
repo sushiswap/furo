aeac9a9da8a90b0f115f7f685722064ba0bc6218
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-28 15:18:30 -0400

    Update FuroStream.sol
    🥢

--- contracts/base/FuroStream.sol
+++ contracts/base/FuroStream.sol
@@ -6,7 +6,7 @@ import "../interfaces/IFuroStream.sol";
 
 contract FuroStream is
     IFuroStream,
-    ERC721("Furo Stream", "FUROSTREAM"),
+    ERC721Permit("Furo Stream", "FUROSTREAM"),
     Multicall,
     BoringOwnable
 {
